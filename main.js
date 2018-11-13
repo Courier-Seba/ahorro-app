d01516e275a15a59b6b74c94b52ed19eefd97acc
Seba <sebastianvaldivita@gmail.com>
2018-11-13 18:47:05 -0300

    Paga a css grid

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+Vue.component('titulo', {
+    template: 
+        "<div id=titulo> <h1 >Calculadora de Ahorro</h1> </div>",
+});
 
+Vue.component('dias', {
+    template: "<input type='text' value=0>"
+});
+
+// Root
 var vm = new Vue({
     el: "#app",
 
     data: {
-        tit1: "Ahorro",
-        tit2: "Calculadora",
         cambia: true
     },
 
